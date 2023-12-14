bed08003bdab5532ed7e5562c12d62228195ec4f
ankush3681 <ankushkumar83623@gmail.com>
2023-12-14 12:28:12 +0530

    done

--- Competishun/foods/src/components/Login.jsx
+++ Competishun/foods/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { signInWithPopup } from 'firebase/auth';
 import React from 'react'
 import { auth, googleAuthProvider } from '../firebase';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "../style/Login.css";
 import { FcGoogle } from "react-icons/fc";
 
@@ -12,24 +12,22 @@ const Login = () => {
           try {
             const result = await signInWithPopup(auth,googleAuthProvider);
             console.log(result)
-            navigate("/recipe")
-              alert("Login Successfully!")
+            navigate("/recipe");
+            alert("Login Successfully!")
           } catch (err) {
             console.log(err);
           }
     }
-
   return (
     <div className='login-container'>
        <div className='login-box'>
-        <h1>SignIn Here</h1>
-        <button onClick={handleClick}><FcGoogle size={35}/> SignIn With Google</button>
+        <h1>Sign In Here</h1>
+        <p>Sign In Here with google</p>
+        <button onClick={handleClick}><FcGoogle size={35} className='googleIcon'/> Sign In With Google</button>
+        <p>Don't have an account? <Link className='signup'>SignUp</Link></p>
        </div>
     </div>
   )
 }
 
 export default Login;
-
-
- 
